065f9d1d332ac7c284eef483f5bcda798d544daa
philipmo <philipmo59@gmail.com>
2024-04-03 22:56:43 -0400

    first push

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
-}
+  return (
+    <div>
+      <h1>Hello from Project Client</h1>
+      <h1>Baby Names</h1>
+    </div>
+)}
 
 export default App;
---
+++ server/migrations/versions/a70cbd3bf3ff_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: a70cbd3bf3ff
+Revises: 
+Create Date: 2024-04-03 22:10:25.935503
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a70cbd3bf3ff'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
